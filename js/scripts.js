43e626b947a1fc642de7594ff747d5a0a4591b67
Yonkokilasi <yonkokilasi@gmail.com>
2017-03-17 16:51:52 +0300

    third

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-var apiKey = require('./../.env').apiKey;
-Look = function () {
-
-}
-Look.prototype.getUser = function (user, displayFunction) {
-    .get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function (response) {
-
-        displayFunction(user, response[0].name);
-    }).fail(function (error) {
-        $("#founduser")
-            .text(error.responseJSON.message);
-    })
-};
-exports.lookModule = Look;
+//var apiKey = require('./../.env').apiKey;
+//Look = function () {
+//
+//}
+//Look.prototype.getUser = function (user, displayFunction) {
+//    .get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function (response) {
+//
+//        displayFunction(user, response[0].name);
+//    }).fail(function (error) {
+//        $("#founduser")
+//            .text(error.responseJSON.message);
+//    })
+//};
+//exports.lookModule = Look;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,13 +1,17 @@
-var Lookup = require('./../js/scripts.js').lookModule;
+var Lookup = require('./../js/scripts.js').lookupModule;
 var apiKey = require('./../.env').apiKey;
-var displayUser = function (user, userData) {
-    $("#founduser").text("You searched for " + user + "Here are their repos" + userData)
-}
+
 $(document).ready(function () {
     $("#blanks").submit(function (event) {
         event.preventDefault();
         var user = $("#lookup").val();
-        currentLookObject.getUser(user, displayUser);
+        $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function (response) {
+            console.log(response);
+            $("#founduser").text("You searched for" + response[0].description)
+        }).fail(function (error) {
+            $("#founduser")
+                .text(error.responseJSON.message);
+        })
     });
 
 });
