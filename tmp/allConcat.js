6e29c4d15b2e3389819f64e671909656d0ae2b07
Yonkokilasi <yonkokilasi@gmail.com>
2017-03-21 09:45:52 +0300

    final

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -2,17 +2,40 @@ var Lookup = require('./../js/scripts.js').lookupModule;
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function () {
+
     $("#blanks").submit(function (event) {
         event.preventDefault();
         var user = $("#lookup").val();
+        $("#lookup").val("");
         $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function (response) {
             console.log(response);
+            $("#search").hide();
             for (var i = 0; i < response.length; i++) {
+                $("searches").prepend("These are the projects for " + user);
                 $("#founduser").append("<li>You searched for " + response[i].name + "</li>")
             }
+
         }).fail(function (error) {
-            $("#founduser")
-                .text(error.responseJSON.message);
+            $("#founduser")     .append(error.responseJSON.message);
+        });
+        $("#searcha").show();
+        $("#searcha").click(function (event) {
+            $("ul li").remove();
+            event.preventDefault();
+            var user = $("#lookup").val();
+            $("#lookup").val("");
+            $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function (response) {
+                console.log(response);
+                $("#search").hide();
+                for (var i = 0; i < response.length; i++) {
+                    $("searches").prepend("These are the projects for " + user);
+                    $("#founduser").append("<li>You searched for " + response[i].name + "</li>")
+                }
+
+            }).fail(function (error) {
+                $("#founduser")
+                    .append(error.responseJSON.message);
+            });
         });
     });
 });
